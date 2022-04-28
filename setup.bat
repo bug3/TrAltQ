aaec90dccb663056081df7996615ee1c14bd1743
bug3 <bug3dev@gmail.com>
2022-04-28 21:11:27 +0300

    Define check_admin

--- setup.bat
+++ setup.bat
@@ -2,3 +2,12 @@
 
 call :check_admin
 exit /B %ERRORLEVEL%
+
+:check_admin
+  	fsutil dirty query %systemdrive% > nul
+  	if %errorlevel% neq 0 (
+    		call :show_admin_control
+  	) else (
+    		call :main
+  	)
+exit /B 0
