74b781d9172939fc6f4555bf6a9b18adfec6fc3f
bug3 <bug3dev@gmail.com>
2022-04-29 19:25:32 +0300

    Add newLang

--- change-key-layout.ps1
+++ change-key-layout.ps1
@@ -2,3 +2,6 @@
 
 $inputTip = Compare-Object (Get-Content imt.txt) (Get-Content imt2.txt) | Select-Object -ExpandProperty InputObject
 $newLang = New-WinUserLanguageList tr
+
+$newLang[0].InputMethodTips.Clear()
+$newLang[0].InputMethodTips.Add($inputTip)
