53064789197e299afa34dd0cb21e87424c3e7ad4
bug3 <bug3dev@gmail.com>
2022-04-29 19:25:00 +0300

    Define vars

--- change-key-layout.ps1
+++ change-key-layout.ps1
@@ -1,1 +1,4 @@
 (Get-WinUserLanguageList).InputMethodTips > imt2.txt
+
+$inputTip = Compare-Object (Get-Content imt.txt) (Get-Content imt2.txt) | Select-Object -ExpandProperty InputObject
+$newLang = New-WinUserLanguageList tr
