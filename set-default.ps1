097439a016df7931c236d2b8831ddd90b9a75a8b
bug3 <bug3dev@gmail.com>
2022-04-30 12:30:52 +0300

    Set tr lang

--- set-default.ps1
+++ set-default.ps1
@@ -1,3 +1,8 @@
 $inputTip = "041F:0000041F"
+$newLang = New-WinUserLanguageList tr
 
+$newLang[0].InputMethodTips.Clear()
+$newLang[0].InputMethodTips.Add($inputTip)
+
+Set-WinUserLanguageList $newLang -Force
 Set-WinDefaultInputMethodOverride -InputTip $inputTip -Force
