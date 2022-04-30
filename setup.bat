d2922e776bcb660f28fb193d42035ae1899613ff
bug3 <bug3dev@gmail.com>
2022-04-30 13:39:52 +0300

    Use goto instead of call

--- setup.bat
+++ setup.bat
@@ -1,16 +1,15 @@
 @echo off
 
-call :check_admin
-exit /B %ERRORLEVEL%
+goto check_admin
 
 :check_admin
   	fsutil dirty query %systemdrive% > nul
   	if %errorlevel% neq 0 (
-    		call :show_admin_control
+    		goto show_admin_control
   	) else (
-    		call :main
+    		goto main
   	)
-exit /B 0
+exit /B
 
 :show_admin_control
 	echo set prompt = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
@@ -18,7 +17,7 @@ exit /B 0
 
   	"%temp%\getadmin.vbs"
   	del "%temp%\getadmin.vbs"
-exit /B 0
+exit /B
 
 :main
 	cd %~dp0
@@ -26,4 +25,4 @@ exit /B 0
 	Powershell.exe -Executionpolicy bypass -File save-input-methods.ps1
 	build\setup.exe
 	Powershell.exe -Executionpolicy bypass -File change-key-layout.ps1
-exit /B 0
+exit /B
